7ebe55166e2ba30884be101ec7cfc6a61c58e516
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-27 18:38:14 +0100

    Add files via upload

---
+++ error/errors.php
@@ -0,0 +1,11 @@
+<br>
+<br>
+<br>
+<br>
+<div class="container py-5 " align="center">
+
+    <img src="../assets/img/logos/fail.svg" style="width: 400px;height:300px;" alt="" srcset="">
+    <br>
+    <br>
+    <h1>Connection Failed</h1>
+</div>
